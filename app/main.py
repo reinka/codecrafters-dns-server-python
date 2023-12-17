b8de7333d51cdf701a69033d7dac8b372f0a125e
Andrei Poehlmann <andrei.poehlmann@sap.com>
2023-12-17 13:39:20 +0100

    add header

--- app/main.py
+++ app/main.py
@@ -1,4 +1,32 @@
 import socket
+import struct
+
+class DNSHeader:
+    def __init__(self):
+        self.id = 1234
+        self.qr = self.opcode = self.aa = self.tc = self.rd = self.ra = self.z = self.rcode = 0
+        self.qdcount = self.ancount = self.nscount = self.arcount = 0
+
+    def to_bytes(self) -> bytes:
+        flags = (
+            (self.qr << 15)
+            | (self.opcode << 11)
+            | (self.aa << 10)
+            | (self.tc << 9)
+            | (self.rd << 8)
+            | (self.ra << 7)
+            | (self.z << 4)
+            | (self.rcode)
+        )
+        return struct.pack(
+            ">HHHHHH",
+            self.id,
+            flags,
+            self.qdcount,
+            self.nscount,
+            self.nscount,
+            self.arcount,
+        )
 
 
 def main():
@@ -10,10 +38,12 @@ def main():
         while True:
             try:
                 data, addr = s.recvfrom(1024)
-                print(f'Received data from {addr}')
-                s.sendto(data, addr)
+                print(f'Received data from {addr}: {data}')
+                header = DNSHeader()
+                header_bytes = header.to_bytes()
+                s.sendto(header_bytes, addr)
             except Exception as e:
-                print(f'Error receiving data {e}')
+                print(f'Error receiving data: {e}')
                 break
 
 if __name__ == "__main__":
